faef3ef4458616b0b628a230f9b62d76872e179a
Kamal19612 <ouedraogokamalelise@gmail.com>
2025-06-18 09:29:28 +0000

    initialisation

---
+++ src/test/java/com/e_defence/gestionvisite/GestionvisiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_defence.gestionvisite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionvisiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
