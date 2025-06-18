5ad65f73b48bf7ed46beb49287c33f35cf696580
Kamal19612 <ouedraogokamalelise@gmail.com>
2025-06-18 09:29:28 +0000

    ajoutFichierPDF

---
+++ src/main/java/com/e_defence/gestionvisite/GestionvisiteApplication.java
@@ -0,0 +1,13 @@
+package com.e_defence.gestionvisite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionvisiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionvisiteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/gestion/controller/ClientController.java
@@ -0,0 +1,10 @@
+package gestion.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (path = "client")
+public class ClientController {
+
+}
