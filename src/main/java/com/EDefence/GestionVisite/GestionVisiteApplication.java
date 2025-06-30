e75a434e9a0242b671a9703faa11269119b610d6
Kamal19612 <ouedraogokamalelise@gmail.com>
2025-06-30 22:11:21 +0000

    initialisation du projet

---
+++ src/main/java/com/EDefence/GestionVisite/GestionVisiteApplication.java
@@ -0,0 +1,13 @@
+package com.EDefence.GestionVisite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVisiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionVisiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EDefence/GestionVisite/GestionVisiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EDefence.GestionVisite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVisiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
